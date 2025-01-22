a5da9b9362bdfb746bacc19e9b7e2ffe07d6dd52
JoseVieira <vieiraja4@gmail.com>
2025-01-22 13:23:19 +0100

    22/01/25

---
+++ 07_APIS/Estudios/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estudios</title>
+</head>
+<body>
+    <?php
+        $apiURL = "http://localhost/Ejercicios/07_APIS/Estudios/api_estudios.php";
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $apiURL);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $respusta = curl_exec($curl);
+        curl_close($curl);
+
+        $estudios = json_decode($respusta, true);
+        print_r($estudios);
+    ?>
+    <table>
+        <thead>
+            <tr>
+                <th>Estudios</th>
+                <th>Ciudad</th>
+                <th>Año fundación</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                foreach($estudios as $estudio){ ?>
+                <tr>
+                    <td><?php echo $estudio["nombre_estudio"]?></td>
+                    <td><?php echo $estudio["ciudad"]?></td>
+                    <td><?php echo $estudio["anno_fundacion"]?></td>
+                </tr>
+                <?php } ?>
+        </tbody>
+    </table>
+</body>
+</html>
