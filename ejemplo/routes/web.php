5862d70f6a6ac64e43969d3c94ad037eb406dab4
JoseVieira <vieiraja4@gmail.com>
2025-02-12 11:04:21 +0100

    Todo correcto con Laravel

--- ejemplo/routes/web.php
+++ ejemplo/routes/web.php
@@ -16,7 +16,8 @@ use App\Http\Controllers\MarcaController;
 |
 */
 
-Route::get('/marcas', [MarcaController::class, 'index']);
+//Route::get('/marcas', [MarcaController::class, 'index']);
+Route::resource('marcas', MarcaController::class);
 
 Route::get('/coches' , [CocheController::class, 'index']);
 
